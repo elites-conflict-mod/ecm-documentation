e25d3aee6d66c789d1e16dfb3633cee95873d7d2
sanick2 <33843199+sanick2@users.noreply.github.com>
2024-09-12 21:16:01 -0500

    add sitemap support
    added sitemap to website

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlightBlog from 'starlight-blog';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://ecm-documentation.netlify.app',
 	integrations: [
 		starlight({
 			plugins: [starlightBlog({
