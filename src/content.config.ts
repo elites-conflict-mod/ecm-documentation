30ba74715a9209306bf8a58c797a9a127d8586be
sanick2 <33843199+sanick2@users.noreply.github.com>
2024-12-18 17:12:41 -0600

    upgrade to Astro v5
    upgrade to Astro v5 to align with new Content Layer API

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection } from 'astro:content';
+import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 import { blogSchema } from 'starlight-blog/schema';
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema({ extend: (context) => blogSchema(context) }) }),
+	docs: defineCollection({ loader: docsLoader(), schema: docsSchema({ extend: (context) => blogSchema(context)}) }),
 };
